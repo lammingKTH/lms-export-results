406c204754ea3bdc66c2d198fc5000f0abbf02be
Rasmus Kaj <kaj@kth.se>
2017-09-29 19:51:44 +0200

    Get and log some data.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 function exportResults (req, res) {
-  res.attachment('file.csv') // Note: course code etc in filename would be nice
+  console.log(req.body)
+  var b = req.body
+  // var course = b.context_label
+  var courseRound = b.lis_course_offering_sourcedid
+  // var canvasCourseId = b.custom_canvas_course_id
+  res.attachment(`${courseRound}-results.csv`)
   res.status(200).send('namn,pnr\nkalle,7207xy')
 }
 
