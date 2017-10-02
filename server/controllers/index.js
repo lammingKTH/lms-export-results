e5b5ec361be343ba07ecfed2e80eabe1010b947f
Elena Rakhimova <elenara@kth.se>
2017-10-02 11:28:06 +0200

    Added redirect urls and fixed settings import

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,21 @@
 'use strict'
+const querystring = require('querystring');
+const settings = require('../configuration').server
+const local = require('../../config/localSettings')
+
 
 function exportResults (req, res) {
   console.log(req.body)
-  var b = req.body
+  let b = req.body
   // var course = b.context_label
-  var courseRound = b.lis_course_offering_sourcedid
-  // var canvasCourseId = b.custom_canvas_course_id
-  res.attachment(`${courseRound}-results.csv`)
-  res.status(200).send('namn,pnr\nkalle,7207xy')
+  let courseRound = b.lis_course_offering_sourcedid
+  const canvasCourseId = b.custom_canvas_course_id
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+  const nextUrl = fullUrl+'2?'+querystring.stringify({courseRound, canvasCourseId })
+  const basicUrl = `https://${settings.canvas_host}/login/oauth2/auth?`+querystring.stringify({client_id: local.client_id, response_type: 'code', redirect_uri: nextUrl})
+  res.redirect(basicUrl)
+  //res.attachment(`${courseRound}-results.csv`)
+  //res.status(200).send('namn,pnr\nkalle,7207xy')
 }
 
 module.exports = {
