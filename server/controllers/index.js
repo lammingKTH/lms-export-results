1be65ad44e6b8b0e38b6b09b7a57a2b9ce72530b
Elena Rakhimova <elenara@kth.se>
2017-10-03 13:53:08 +0200

    fixed linting

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -12,7 +12,6 @@ const ldapClient = Promise.promisifyAll(ldap.createClient({
   url: local.ugUrl
 }))
 
-
 function exportResults (req, res) {
   let b = req.body
   // console.log(b)
@@ -25,12 +24,12 @@ function exportResults (req, res) {
   res.redirect(basicUrl)
 }
 
-async function exportResults2 (req, res) { 
+async function exportResults2 (req, res) {
   let courseRound = req.query.courseRound
   const canvasCourseId = req.query.canvasCourseId
   console.log(`Should export for ${courseRound} / ${canvasCourseId}`)
   try {
-    await ldapClient.bindAsync(local.ugUsername, local.ugPwd)    
+    await ldapClient.bindAsync(local.ugUsername, local.ugPwd)
     const auth = await rp({
       method: 'POST',
       uri: `https://${settings.canvas_host}/login/oauth2/token`,
@@ -43,32 +42,23 @@ async function exportResults2 (req, res) {
       },
       json: true
     })
-    // console.log(auth)
     const canvasApi = new CanvasApi(`https://${settings.canvas_host}/api/v1`, auth.access_token)
     const assignments = await canvasApi.requestCanvas(`courses/${canvasCourseId}/assignments`)
-    console.log('=======================================================================')
-    // console.log(assignments)
     const assignmentIds = []
     const headers = {}
     for (let t of assignments) {
       const id = '' + t.id
       assignmentIds.push(id)
       headers[id] = `${t.name} (${t.id})`
-      // console.log(`${t.id} is "${t.name}"`)
     }
-    console.log('-----------------------------------------------------------------------')
     const csvHeader = ['SIS User ID', 'Name', 'Surname', 'PersonNummer'].concat(assignmentIds.map(function (id) { return headers[id] }))
-    // console.log(csvHeader)
-    console.log('-----------------------------------------------------------------------')
     const students = await canvasApi.requestCanvas(`courses/${canvasCourseId}/students/submissions?grouped=1&student_ids[]=all`)
     // So far so good, start constructing the output
     res.status(200)
     res.contentType('csv')
     res.attachment(`${courseRound || 'canvas'}-results.csv`)
     res.write(csv.createLine(csvHeader))
-    
     const attributes = ['givenName', 'sn', 'norEduPersonNIN']
-    
     for (let student of students) {
       const ldapResults = await ldapClient.searchAsync('ou=UG,dc=referens,dc=sys,dc=kth,dc=se', {
         scope: 'sub',
@@ -81,32 +71,28 @@ async function exportResults2 (req, res) {
           pagePause: false
         }
       })
-      const ugUser = await new Promise((resolve,reject) => {
+      const ugUser = await new Promise((resolve, reject) => {
         const user = []
         ldapResults.on('searchEntry', ({object}) => user.push(object))
         ldapResults.on('end', () => resolve(user))
         ldapResults.on('error', reject)
       })
-      console.log('ugUser::::::',ugUser)
-
       let row = {
-        kthid: student.sis_user_id,
+        kthid: student.sis_user_id
       }
       if (ugUser.length > 0) {
-        row['givenName'] = ugUser[0].givenName,
-        row['surname'] = ugUser[0].sn,
+        row['givenName'] = ugUser[0].givenName
+        row['surname'] = ugUser[0].sn
         row['personnummer'] = ugUser[0].norEduPersonNIN
       }
       for (let submission of student.submissions) {
-        row['' + submission.assignment_id] = submission.entered_grade || ''
+        row[ '' + submission.assignment_id ] = submission.entered_grade || ''
       }
       const csvLine = [student.sis_user_id || student.id, row['givenName'] || '', row['surname'] || '', row['personnummer'] || ''].concat(assignmentIds.map(function (id) { return row[id] || '-' }))
       // console.log(csvLine)
       res.write(csv.createLine(csvLine))
     }
-    console.log('-----------------------------------------------------------------------')
     res.send()
-
   } catch (e) {
     console.log(e)
     res.status(500).send('Trasigt')
