35f0d8d672ba0f0ebfb3e97545c7fc76af399cd3
Emil Stenberg <esandin@gmail.com>
2017-10-06 11:24:39 +0200

    removed log conf

--- server/log.js
+++ server/log.js
@@ -2,9 +2,9 @@ const bunyan = require('bunyan')
 const packageFile = require('../package.json')
 
 module.exports = bunyan.createLogger({
-  name:'node-logger',
-  app: packageFile.name,
-  serializers: {
-    err: bunyan.stdSerializers.err
-  }
+  name: 'node-logger',
+  app: packageFile.name
+  // serializers: {
+  //   err: bunyan.stdSerializers.err
+  // }
 })
