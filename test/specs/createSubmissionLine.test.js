48c83599a4f15db1091c505019bc75dbdff3b041
Emil Stenberg <esandin@gmail.com>
2017-10-11 12:43:39 +0200

    added stub for test function

--- test/specs/createSubmissionLine.test.js
+++ test/specs/createSubmissionLine.test.js
@@ -1,8 +1,18 @@
 const test = require('tape')
 const rewire = require('rewire')
 const indexController = rewire('../../server/controllers')
-
+const createSubmissionLine = indexController.__get__('createSubmissionLine')
 test('should do something', t => {
   t.plan(1)
+  const student = {}
+  const assignmentIds = []
+  const ldapClient = {lookupUser: () => Promise.resolve({
+    givenName: 'Mock name',
+    sn: 'Mock surname',
+    norEduPersonNIN: '123123123123'
+  })}
+
+  const result = createSubmissionLine({student, ldapClient, assignmentIds})
+
   t.equal(1, 1)
 })
