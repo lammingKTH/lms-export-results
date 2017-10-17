0839eb2cb2a6a69178145111f82342f3623e04c9
Emil Stenberg <emilsten@kth.se>
2017-10-17 10:38:26 +0200

    refactor

--- server/controllers/csvFile.js
+++ server/controllers/csvFile.js
@@ -8,10 +8,8 @@ function escapeCsvData (str) {
   return str
 }
 
-function createLine (strArr) {
-  return strArr.map(escapeCsvData).join(';') + '\n'
-}
-
 module.exports = {
-  createLine
+  createLine(strArr){
+      return strArr.map(escapeCsvData).join(';') + '\n'
+  }
 }
