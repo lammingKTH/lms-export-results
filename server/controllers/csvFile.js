01b43dc7b2d5a1a6b0f5ebbc2b150a771ba7c2b8
Emil Stenberg <emilsten@kth.se>
2017-10-17 10:35:54 +0200

    removed log line

--- server/controllers/csvFile.js
+++ server/controllers/csvFile.js
@@ -1,12 +1,7 @@
 function escapeCsvData (str) {
   str = '' + str
 
-  if (str.includes('"')) {
-    console.warn('oh no! bad data!', str)
-  }
-
   if (str.includes(';')) {
-    console.log('escaping ', str)
     str = `"${str}"`
   }
 
