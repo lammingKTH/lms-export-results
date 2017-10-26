3e4fd4473bf4a9691fa2d7935d7bce2b6bb300eb
Emil Stenberg <emilsten@kth.se>
2017-10-26 14:49:08 +0200

    logging in and clicking on /courses

--- cypress/integration/export.test.js
+++ cypress/integration/export.test.js
@@ -1,14 +1,19 @@
-describe('Export', ()=>{
-  it('should download a csv file', ()=>{
-      cy.visit('https://kth.test.instructure.com/login/canvas')
+function login () {
+  cy.visit('https://kth.test.instructure.com/login/canvas')
 
-      cy.get('#pseudonym_session_unique_id')
-      .type(Cypress.env('CANVAS_TESTUSER_USERNAME'))
+  cy.get('#pseudonym_session_unique_id')
+  .type(Cypress.env('CANVAS_TESTUSER_USERNAME'))
 
-      cy.get('#pseudonym_session_password')
-      .type(Cypress.env('CANVAS_TESTUSER_PASSWORD'))
+  cy.get('#pseudonym_session_password')
+  .type(Cypress.env('CANVAS_TESTUSER_PASSWORD'))
 
-      cy.get('#login_form .Button--login').click()
+  cy.get('#login_form .Button--login').click()
+}
 
+describe('Export', () => {
+  it('should download a csv file', () => {
+    login()
+    cy.get('#global_nav_courses_link').click()
+    cy.get('.ic-NavMenu-list-item__link:first').click()
   })
 })
