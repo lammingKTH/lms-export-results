d499579fe46df8277987f7917dcdd9d4aef30819
Emil Stenberg <emilsten@kth.se>
2017-11-08 14:20:18 +0100

    changed url in test

--- cypress/integration/export.test.js
+++ cypress/integration/export.test.js
@@ -14,7 +14,7 @@ describe('Export', () => {
   it('should download a csv file', () => {
     login()
     // log.info(`The user ${b.lis_person_sourcedid}, ${b.custom_canvas_user_login_id}, is exporting the course ${b.context_label} with id ${b.custom_canvas_course_id}`)
-    cy.visit('https://kth.test.instructure.com/courses/4/external_tools/490?display=borderless')
+    cy.visit('https://kth.test.instructure.com/courses/4/external_tools/489?display=borderless')
     cy.contains('Godkänn').click()
     // The download should now start. But we need to verify that the file gets downloaded
 
