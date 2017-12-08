73d523b057275eb8785de9aecdac49fa69d5cc97
Emil Stenberg <esandin@gmail.com>
2017-12-08 13:39:39 +0100

    wip: adding one unit test

--- test/specs/export.test.js
+++ test/specs/export.test.js
@@ -4,6 +4,7 @@ const sinon = require('sinon')
 
 const _export = rewire('../../server/export')
 const exportResults = _export.__get__('exportResults')
+const exportResults3 = _export.__get__('exportResults3')
 
 test('should redirect to the Canvas authentication page', t => {
   const res = {redirect: sinon.spy()}
@@ -17,7 +18,7 @@ test('should redirect to the Canvas authentication page', t => {
 
 test('should send status:500 if exportResults breaks', t => {
   const res = {status: sinon.stub().returns({
-    send(){}
+    send () {}
   })}
   const req = {body: {}, get: () => {
     throw new Error('Just pretending that something breaks...')
@@ -27,5 +28,15 @@ test('should send status:500 if exportResults breaks', t => {
 
   t.equal(res.status.getCalls()[0].args[0], 500)
 
+  t.end()
+})
+
+test.only('should write a file', t => {
+  const res = {redirect: sinon.spy()}
+  const req = {query: {courseRound: 'round', canvasCourseId: 'canvasCourseId'}, get: () => ''}
+
+  exportResults3(req, res)
+
+  t.equal(res.redirect.callCount, 1)
   t.end()
 })
